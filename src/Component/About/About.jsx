d53ffd18370f7dcc32f39719bb1a02cb8af5ae81
tenten039 <nhoxcon029@gmail.com>
2022-07-18 22:09:19 +0700

    first commit

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -5,7 +5,7 @@ import { FaCode, FaReact, FaHtml5, FaCss3Alt, FaBirthdayCake, FaUserAlt, FaSchoo
 import { SiJavascript } from 'react-icons/si'
 import { IoIosSchool } from "react-icons/io";
 import Typewriter from 'typewriter-effect';
-
+import DownloadLink from "react-download-link";
 import Loader from '../Loader/Loader';
 
 import './About.css'
@@ -87,7 +87,13 @@ function About() {
                             I always to give my best to help the company grow and achieve for myself
                         </p>
                         <button className="btn-down" >
-                            <a className="link-down" href={"./Mycv.pdf"} download="Mycv.pdf">Download My CV</a>
+                        {/* <DownloadLink
+                            label="Save"
+                            filename="Mycv.pdf"
+                            exportFile={() => Promise.resolve("./Mycv.pdf")}
+
+                        /> */}
+                            <a className="link-down" href={"https://doc-10-c0-docs.googleusercontent.com/docs/securesc/vhlq2itb8kllkrd8e4lfl0mma45naq8j/67v3qcjrasskvefsgh79f3qas3eksf6a/1658156775000/11805274766228785216/11805274766228785216/12GBxhXTQI1OwOIEbEhP9y7C8yfGjZrpz?e=download&ax=ACxEAsavyzYGBW8sYvbQFP_8BH875eKgWmHkql64g-LRtKpMAEPBt9dJQ36WDnpSs8SvsqDLWhitrh-035NI85-RMYf7C31YGECBkwNYUVqFobcjaZMDTzdpF1tQbcasO6GTUBErYr8Xk7a3_ZrQdcmsUU4zPczy5DStWBXCTgkRaeiPwk05ibyPDyfxVjC0cTIk6ergQ7U7OHNUKJzYesptq-WohZ5fiU1wa1QabAV813imivT6H08DYEWPq0bpVr3v4BvxmG8qm2_IQetN1TaYEj-eoIdlNujYEc5q7dtt5SuwuWXmlVazkjRZCkRrGkFfH1_G0Ma6Q1sqQKYx1fhX-2Hffve5rmtigfx60mP7xM1RrAfjVKuvriyCnJEmeQcFc4UKjj40mFobgQv9Ah7XZT4144KwNemW5uI132sgNUvElrjwPuGLuEsPz05rs-eJzo5kn8-X-0sAOr5LhOMx2CEvwon5WZWfHMM_tLsuXPNJu_CWDZTNlkb0ae9ZBkoPbFo39FNloi2tQWjziBoG1QRSclSm6eFCg95F7LX6luwaG1coosClXlkdkja8QRPj2si32-aSSs3w52Ze3uw7L-voftABMnoqumChEXQti9gYhV438KhhFaYPwTvkPDFneEwjxfSlpXjhzjG7_W1ZqahY1lg6SXb_2g7tC4jAX4MZ3yzEYXhEg7wkfLjtDh2bdSEFkXn4TzrqADNXBIfdrCXpmOfOVuyO8eR3h-ib3jAjWliVdKjT9PaMA8O42N9rK_ATNojNFKN6cZmSaHg..."} target="_blank" download>Download My CV</a>
                         </button>
                     </div>
                 </section>
